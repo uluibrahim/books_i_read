a3a977bf7b96fcfffdda4efa41749aa6fe2c726c
uluibrahim <iulu17@posta.pau.edu.tr>
2022-04-17 20:59:22 +0300

    Uygulama sabitlerinin tutulacağı dosyayıo ekler

---
+++ lib/core/constants/app/application_constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ApplicationConstants {
+  static const appName = 'Kitaplarım';
+  static const langAssetPath = 'assets/language';
+  static const appWebSite = '';
+  static const email = "";
+
+  static const trLocale = Locale("tr", "TR");
+  static const supportedLocale = [ApplicationConstants.trLocale];
+}
